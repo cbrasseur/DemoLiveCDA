d08f6502c328eef4e1d3d4c53ab792062eed6033
Cédric BRASSEUR <Cdric.brasseur@gmail.com>
2024-02-01 14:24:26 +0100

    ✨  Add initial sample project

---
+++ DemoLiveDebuggingCDA/DemoLiveDebuggingCDA/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+List<string> lstElements = new List<string>
+{
+    "Bonjour",
+    "Bonsoir",
+    "Test"
+};
+
+bool isTrue = true;
+
+if (isTrue)
+    DisplayElements(lstElements);
+else
+    Console.WriteLine("Boolean is not true, no display");
+
+Random rand = new Random();
+var randomValue = rand.Next(1, 100);
+
+Console.WriteLine("Nombre random {0}", randomValue);
+
+void DisplayElements(List<string> lstElements)
+{
+    int count = 0;
+
+    foreach (string element in lstElements)
+    {
+        count++;
+        Console.WriteLine("Element n°{0}", count);
+        Console.WriteLine("L'elément est {0}", element);
+    }
+}
